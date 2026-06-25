bea3213b955fca6f280432e8a4cddb6023f4192f
Matt Rabah <matt@cadencelab.co>
2026-06-24 23:01:10 -0400

    feat: add admin dashboard and associated assets to public directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tina from '@tinacms/astro/integration';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [
     tina()
   ]
