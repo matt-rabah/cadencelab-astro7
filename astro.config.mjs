aa77e8508c0c2578e0b145b265dedcc6e503e116
Matt Rabah <matt@cadencelab.co>
2026-06-26 01:30:39 -0400

    refactor: update TinaCMS schema configuration and project structure for admin integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tina from '@tinacms/astro/integration';
-import node from '@astrojs/node';
+import { defineConfig } from "astro/config";
+import tina from "@tinacms/astro/integration";
+import node from "@astrojs/node";
 
 export default defineConfig({
   adapter: node({
-    mode: 'standalone'
+    mode: "standalone",
   }),
-  integrations: [
-    tina()
-  ]
+  integrations: [tina()],
 });
