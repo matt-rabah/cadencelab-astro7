9b04608e45b596ab9ec409e8654c401725580388
Matt Rabah <matt@cadencelab.co>
2026-07-13 20:58:10 -0400

    feat: add responsive navigation header with search and mega menu functionality

--- .history/public/admin/assets/architecture-TIHT7OUA-1982357c 2_20260626110444.js
+++
@@ -1,1 +0,0 @@
-import{A as c,f as i}from"./mermaid-parser.core-533b2f20.js";import"./index-a21b20f3.js";export{c as ArchitectureModule,i as createArchitectureServices};
--- .history/tina/__generated__/client_20260713203810.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types.js";
-export const client = createClient({
-  url: "http://localhost:4001/graphql",
-  token: "79c07460d61d4534d4e576c7bf8eb9917ed00cae",
-  queries,
-});
-export default client;
